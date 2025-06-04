1dc2479b3b9ae8199db15cfaaf8d2006b0ff03b3
marJoNdan <148409999+marJoNdan@users.noreply.github.com>
2025-06-04 14:05:52 -0500

    Push after commit new changes

--- Iris Flower Dataset Demo.py
+++ Iris Flower Dataset Demo.py
@@ -16,7 +16,13 @@ iris.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 't
 #outputs = clf.predict(x_test)
 #outputs
 
-#accuracy_score = 
 #from sklearn.metrics import accuracy_score
-#performance = accuracy_score(y_test, outputs)
-#print(f"Accuracy: {performance*100:.2f}%")
+performance = accuracy_score(y_test, outputs)
+print(f"Accuracy: {performance*100:.2f}%")
+
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+
+X, _ = make_blobs(n_samples = 256, centers = 3, cluster_std = 1.0, random_state= 42)
+kmeans = KMeans(n_clusters= 3, random_state= 42)
+kmeans.fit(X)
