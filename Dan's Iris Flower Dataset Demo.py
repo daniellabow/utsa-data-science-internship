ddc73cac325e275bbdf0c0eda70dbce9e9c74bb7
marJoNdan <148409999+marJoNdan@users.noreply.github.com>
2025-06-04 23:03:15 -0500

    Updated, troubleshooted, and fixed code in Dan's Wine file and in Dan's Iris file. Cleaned up and added necessary librarys

--- Dan's Iris Flower Dataset Demo.py
+++ Dan's Iris Flower Dataset Demo.py
@@ -6,32 +6,57 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-iris = pd.read_csv(url, header=None)
+from sklearn.datasets import load_iris
 
-# Add headers manually
-iris.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'target']
+iris = load_iris()
+df_iris = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+df_iris
 
-#0=setosa, 1=versicolor, 2=virginica
+df_iris["target"] = iris.target
+df_iris  #0=setosa, 1=versicolor, 2=virginica
+X_class = df_iris.drop(columns="target")
+y_class = df_iris['target']
 
-x_class = iris.drop(columns="target")
-outputs = plt.clf.predict(_test)
+#first: create data splits
+from sklearn.model_selection import train_test_split
 
-#from sklearn.metrics import accuracy_score
+X_train, X_test, y_train, y_test   = train_test_split(X_class, y_class, test_size=.2)
+#X_train, X_valid, y_valid, y_valid = train_test_split(X_train, y_train, test_size=.1)
+
+#second: train the model on some input data
+from sklearn.linear_model import LogisticRegression
+
+clf = LogisticRegression(max_iter=1000)
+clf.fit(X_train, y_train)
+
+#third: test/eval the model
+outputs = clf.predict(X_test)
+outputs
+
 performance = accuracy_score(y_test, outputs)
 print(f"Accuracy: {performance*100:.2f}%")
 
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 
-X, _ = make_blobs(n_samples = 256, centers = 3, cluster_std = 1.0, random_state= 42)
-kmeans = KMeans(n_clusters= 3, random_state= 42)
+#generate some data
+X, _ = make_blobs(n_samples=256, centers=3, cluster_std=1.0, random_state=42)
+
+#train the model
+kmeans = KMeans(n_clusters=3, random_state=42)
 kmeans.fit(X)
 
+y_kmeans = kmeans.predict(X)
+y_kmeans
+
+centers = kmeans.cluster_centers_
+
 import matplotlib.pyplot as plt
+
 plt.figure(figsize=(4,4))
-plt.scatter(X[:,0], X[:,1], label="random blob points")
-plt.scatter(center[:,0], center[:,1], label="centroids")
+plt.scatter(X[:, 0], X[:, 1], label="random blob points")  #input data
+plt.scatter(centers[:, 0], centers[:, 1], label="centroids")
+
 plt.title("K-Means Clustering Results")
 plt.xlabel("weight (lb)")
 plt.ylabel("height (cm)")
--- Dan's Wine Dataset Demo.py
+++ Dan's Wine Dataset Demo.py
@@ -1,24 +1,26 @@
 from sklearn.datasets import load_wine
-wine = load_wine
+import pandas as pd
+wine   = load_wine()
 X_wine = wine.data
 y_wine = wine.target
+X_wine
 
-df_wine = pd.DataFrame(data=X_wine.data, columns=wine.feature_names)
+df_wine = pd.DataFrame(data=wine.data, columns=wine.feature_names)
 df_wine
 df_wine.describe()
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+
 scaler = StandardScaler()
 wine_scaled = scaler.fit_transform(df_wine)
-help(scaler)
-
 wine_scaled
 
-from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score
 clf_wine = LogisticRegression(max_iter=1000)
 
-cv = StratifiedKFold(n_splits=5, shuffle= True, random_state= 42)
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
 scores = cross_val_score(clf_wine, wine_scaled, y_wine, cv=cv, scoring="accuracy")
 print("CrossVAL Accuracy Scores:", scores)
